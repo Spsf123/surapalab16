cd3c1b23ed8faf8f3bab7a008e4308ae98f49b7a
Surapa <surapa152@gmail.com>
2022-01-18 00:14:55 +0700

    lab 16 completed

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,20 @@ int main(){
 	
 	return 0;
 }
+void shuffle(int &e,int &f,int &g,int &h){
+	int ang[] = { e,f,g,h};
+	int pao[4] = {};
+	for(int i = 0; i < 4; i++){
+		pao[i] = rand()%4;
+		for(int j = 0; j< i; j++){
+			if(pao[i] == pao[j]){
+				i--;
+			}
+		}
+	}	
+int money[4] = { ang[pao[0]],ang[pao[1]],ang[pao[2]],ang[pao[3]]};
+e = money[0];
+f = money[1];
+g = money[2];
+h = money[3];
+}
--- lab16_5.cpp
+++ lab16_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *,int *, int *, int *); //Modify input arguments to pointer 
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,7 +11,7 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
+	    shuffle(&a,&b,&c,&d); //Modify input arguments to pointer 
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
@@ -19,3 +19,20 @@ int main(){
 }
 
 //Write definition of shuffle() using pointer here 
+void shuffle(int* e,int* f,int* g,int * h){
+	int ang[] = { *e,*f,*g,*h};
+	int pao[4] = {};
+	for(int i = 0; i < 4; i++){
+		pao[i] = rand()%4;
+		for(int j = 0; j< i; j++){
+			if(pao[i] == pao[j]){
+				i--;
+			}
+		}
+	}	
+int money[4] = { ang[pao[0]],ang[pao[1]],ang[pao[2]],ang[pao[3]]};
+*e = money[0];
+*f = money[1];
+*g = money[2];
+*h = money[3];
+}
